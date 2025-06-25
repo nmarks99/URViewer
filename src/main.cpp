59fce40b4a689b648b8a5bb3641d5d89a6c1b596
Nick Marks <nmarks99@gmail.com>
2025-06-24 23:32:38 -0500

    moving just joint1 works

--- src/main.cpp
+++ src/main.cpp
@@ -5,37 +5,6 @@
 
 #include "UR3e.hpp"
 
-constexpr Matrix Rx(float th) {
-    using namespace std;
-    return Matrix {
-        1.0,      0.0,     0.0, 0.0,
-        0.0,  cos(th), sin(th), 0.0,
-        0.0, -sin(th), cos(th), 0.0,
-        0.0,      0.0,     0.0, 1.0,
-    };
-}
-
-constexpr Matrix Ry(float th) {
-    using namespace std;
-     return Matrix {
-        cos(th), 0.0, -sin(th), 0.0,
-        sin(th), 1.0,  cos(th), 0.0,
-            0.0, 0.0,      0.0, 0.0,
-            0.0, 0.0,      0.0, 1.0,
-    };
-}
-
-constexpr Matrix Rz(float th) {
-    using namespace std;
-     return Matrix {
-        cos(th), -sin(th), 0.0, 0.0,
-        sin(th),  cos(th), 0.0, 0.0,
-            0.0,      0.0, 1.0, 0.0,
-            0.0,      0.0, 0.0, 1.0,
-    };
-}
-
-
 void draw_world_axes();
 void print_matrix(const Matrix &m) {
     printf("%.4f, %.4f, %.4f, %.4f,\n%.4f, %.4f, %.4f, %.4f,\n%.4f, %.4f, %.4f, %.4f,\n%.4f, %.4f, %.4f, %.4f,\n",
@@ -86,55 +55,50 @@ int main(void) {
 
     // Load models
     Link base("models/Base_UR3.gltf");
-    base.model.transform = TSBASE;
+    base.model.transform = MatrixIdentity();
 
     Link link1("models/Link1_UR3.gltf");
     link1.model.transform = TS1;
 
     Link link2("models/Link2_UR3.gltf");
-    const Matrix T12 = MatrixMultiply(
-        MatrixTranslate(0.0, 0.12, 0.0),
-        MatrixRotateZYX({PI, 0.0, -PI/2})
-    );
     link2.model.transform = MatrixMultiply(T12, link1.model.transform);
+    const Matrix TS2 = link2.model.transform;
     printf("link2.model.transform = \n");
     print_matrix(link2.model.transform);
     printf("\n");
 
     Link link3("models/Link3_UR3.gltf");
-    const Matrix T23 = MatrixMultiply(
-        MatrixTranslate(-0.457, 0.09, 0.0),
-        MatrixRotateXYZ({PI, 0.0, 0.0})
-    );
     link3.model.transform = MatrixMultiply(T23, link2.model.transform);
+    const Matrix TS3 = link3.model.transform;
     printf("link3.model.transform = \n");
     print_matrix(link3.model.transform);
     printf("\n");
 
     Link link4("models/Link4_UR3.gltf");
-    link4.model.transform = MatrixMultiply(MatrixRotateXYZ({0.0, PI, PI/2}), link3.model.transform);
-    link4.model.transform = MatrixMultiply(MatrixTranslate(-0.1, 0.0, 0.0), link4.model.transform);
+    link4.model.transform = MatrixMultiply(T34, link3.model.transform);
+    const Matrix TS4 = link4.model.transform;
     printf("link4.model.transform = \n");
     print_matrix(link4.model.transform);
     printf("\n");
 
     Link link5("models/Link5_UR3.gltf");
-    link5.model.transform = MatrixMultiply(MatrixRotateXYZ({0.0, PI, PI/2}), link4.model.transform);
-    link5.model.transform = MatrixMultiply(MatrixTranslate(-0.085, 0.0, 0.0), link5.model.transform);
+    link5.model.transform = MatrixMultiply(T45, link4.model.transform);
+    const Matrix TS5 = link5.model.transform;
     printf("link5.model.transform = \n");
     print_matrix(link5.model.transform);
     printf("\n");
 
     Link link6("models/Link6_UR3.gltf");
-    link6.model.transform = MatrixMultiply(MatrixRotateXYZ({-PI/2, 0.0, 0.0}), link5.model.transform);
-    link6.model.transform = MatrixMultiply(MatrixTranslate(0.0, 0.0, -0.09), link6.model.transform);
+    link6.model.transform = MatrixMultiply(T56, link5.model.transform);
+    const Matrix TS6 = link6.model.transform;
     printf("link6.model.transform = \n");
     print_matrix(link6.model.transform);
     printf("\n");
 
-    Vector3 j1_trans_vec = extract_translation(link1.model.transform);
-    Matrix j1_trans = MatrixTranslate(j1_trans_vec.x, j1_trans_vec.y, j1_trans_vec.z);
-    float joint1 = 0.0;
+    // Vector3 j1_trans_vec = extract_translation(link1.model.transform);
+    // Matrix j1_trans = MatrixTranslate(j1_trans_vec.x, j1_trans_vec.y, j1_trans_vec.z);
+    float joint1 = PI/4;
+    float joint2 = PI/4;
 
     while (!WindowShouldClose()) {
         if (IsKeyDown(KEY_LEFT_CONTROL)) {
@@ -142,12 +106,13 @@ int main(void) {
         }
 
 
-        link1.model.transform = MatrixMultiply(MatrixRotateY(joint1), j1_trans); // TS1
-
-        // update the child transforms
-        // link2.model.transform = MatrixMultiply(MatrixRotateZYX({PI, 0.0, -PI/2}), link2.model.transform);
-        link2.model.transform = MatrixMultiply(MatrixRotateZYX({PI, 0.0, -PI/2}), link1.model.transform);
-        link2.model.transform = MatrixMultiply(MatrixTranslate(0.0, 0.12, 0.0), link2.model.transform);
+        // update for joint1 changes
+        link1.model.transform = MatrixMultiply(TS1, MatrixRotateY(joint1));
+        link2.model.transform = MatrixMultiply(T12, link1.model.transform);
+        link3.model.transform = MatrixMultiply(T23, link2.model.transform);
+        link4.model.transform = MatrixMultiply(T34, link3.model.transform);
+        link5.model.transform = MatrixMultiply(T45, link4.model.transform);
+        link6.model.transform = MatrixMultiply(T56, link5.model.transform);
 
         // DRAW /////////////////////////////////
         BeginDrawing();
@@ -155,9 +120,9 @@ int main(void) {
 
         GuiSlider((Rectangle){96, 48, 216, 16}, TextFormat("%0.2f", joint1), NULL, &joint1, -2 * 3.14f,
                   2 * 3.14f);
-        // GuiSlider((Rectangle){96, 68, 216, 16}, TextFormat("%0.2f", joint1), NULL, &joint1, -2 * 3.14f,
-                  // 2 * 3.14f);
-        // GuiSlider((Rectangle){96, 88, 216, 16}, TextFormat("%0.2f", rz), NULL, &rz, -2 * 3.14f,
+        GuiSlider((Rectangle){96, 68, 216, 16}, TextFormat("%0.2f", joint2), NULL, &joint2, -2 * 3.14f,
+                  2 * 3.14f);
+        // GuiSlider((Rectangle){96, 88, 216, 16}, TextFormat("%0.2f", joint3), NULL, &joint3, -2 * 3.14f,
                   // 2 * 3.14f);
 
         // 3D -------------
